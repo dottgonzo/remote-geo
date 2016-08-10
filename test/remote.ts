ad8818d2c5da39f899e5dc05aa119d4823aa11ab
dario <darioyzf@gmail.com>
2016-08-10 15:43:15 +0200

    tested auth

--- test/remote.ts
+++ test/remote.ts
@@ -1,5 +1,6 @@
 import Localize from '../index'
 import * as mocha from "mocha";
+import * as superagent from "superagent";
 
 const expect = require("chai").expect;
 
@@ -11,12 +12,15 @@ const pos1 = [37.504192, 15.068489]
 const latlng = { latitude: pos0[0], longitude: pos0[1] };
 const latlng2 = { latitude: pos1[0], longitude: pos1[1] };
 
+let token: any = false;
 
 let loc: Localize;
 
 before(function () {
+
     loc = new Localize({ remote: remote })
 })
+
 describe("test remote server", function () {
 
     describe("class instantiation with standards", function () {
@@ -29,17 +33,24 @@ describe("test remote server", function () {
             expect(loc.state).to.not.exist;
             expect(loc).to.be.ok;
             expect(loc).to.be.an('Object');
-
         })
-
-
     })
 
     describe("position is ok?", function () {
         this.timeout(30000);
 
         it("verificate working of localization", function (done) {
-            loc.setPosition(latlng).then((c) => {
+
+    superagent.get("https://emnetserver.kernel.online/auth/authorize/couchdb/dario/2079666z").end((err, res) => {
+        if (err) {
+            done(Error(err));
+        } else if (res && res.body && !res.body.error) {
+
+            const obj = res.body;
+
+            token = obj.token;
+
+            loc.setPosition(latlng, token).then((c) => {
 
                 expect(loc.state).to.be.an('Object');
                 expect(c).to.be.an('Array');
@@ -48,24 +59,44 @@ describe("test remote server", function () {
                 done()
             }).catch((c) => {
                 done(Error(c))
-
             })
+
+
+        } else {
+            done(Error(res.body.error))
+        }
+    })
+
+
         })
         it("verificate working of 2 localization", function (done) {
 
-            loc.setPosition(latlng2).then((c) => {
+
+    superagent.get("https://emnetserver.kernel.online/auth/authorize/couchdb/dario/2079666z").end((err, res) => {
+        if (err) {
+            done(Error(err));
+        } else if (res && res.body && !res.body.error) {
+
+            const obj = res.body;
+
+            token = obj.token;
+
+            loc.setPosition(latlng2, token).then((c) => {
 
                 expect(loc.state).to.be.an('Object');
                 expect(c).to.be.an('Array');
                 expect(c[0]).to.be.an('Object');
                 expect(c[0].nativeName).to.be.a('string');
-                done();
-
+                done()
             }).catch((c) => {
-
                 done(Error(c))
-
             })
+
+
+        } else {
+            done(Error(res.body.error))
+        }
+    })
 
 
 
